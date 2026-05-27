5d514ba3ba3ab1f23ab77ebe451b7e37e8b40bb6
bhernandez-ext <hernandezjaramillob@gmail.com>
2026-05-26 20:38:16 -0500

    Add configuration for Supabase connection

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.ColombiaEligeConfig = window.ColombiaEligeConfig || {
+  supabaseUrl: "https://oojxejytdabvdipjrkud.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vanhlanl0ZGFidmRpcGpya3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MzU3ODMsImV4cCI6MjA5NTQxMTc4M30.vlb7Tu09hfwrio3H5tIrO7th9y0yFdUzRVVjodBZAiM",
+};
